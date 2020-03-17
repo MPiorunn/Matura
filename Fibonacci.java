8fff2cde9c607470e6d046d1ff6739f180b2f53f
Maciej Piorun <36971527+MPiorunn@users.noreply.github.com>
2020-03-17 12:58:17 +0100

    Dodałem komentarze o Fibonaccim

--- Fibonacci.java
+++ Fibonacci.java
@@ -8,15 +8,23 @@ public class Fibonacci {
 
     }
 
+    // iteracyjna metoda znalezienia n elementów
     private static int[] iterative(int numbers) {
+        
+        // nowa tabalica intów o rozmiarze n
         int[] series = new int[numbers];
         for (int i = 0; i < numbers; i++) {
 
+            // zerowy element jest zawsze zero
             if (i == 0) {
                 series[i] = 0;
-            } else if (i == 1) {
+            } 
+            // pierwszy element jest zawsze 1
+            else if (i == 1) {
                 series[i] = 1;
-            } else {
+            } 
+            // każdy kolejny element jest sumą dwóch poprzednich
+            else {
                 series[i] = series[i - 1] + series[i - 2];
             }
 
@@ -24,10 +32,14 @@ public class Fibonacci {
         return series;
     }
 
+    // metoda rekurencyjna do odnalezienia n-tego elementu ciągu fibonacciego
     public static int recursive(int n) {
+        // jeżeli szukamy wartości elementu zerowego lub pierwszego, możemy po prostu zwrócić n, które jest równe 0 lub 1
+        // to jest bardzo ważne miejsce w którym zatrzymujemy rekurencję żeby nie trwała w nieskończoność
         if (n <= 1) {
             return n;
         }
+        // jeżeli szukamy innego elementu, musimy rekurencyjnie wywołać sumę dwóch poprzednich elementów
         return recursive(n - 1) + recursive(n - 2);
     }
 
