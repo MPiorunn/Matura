a7caf5565293b1a88ab91ae90bcaf9d630340dc5
Maciej Piorun <36971527+MPiorunn@users.noreply.github.com>
2020-03-17 13:00:39 +0100

    Komentarze o algorytmie

--- Prime.java
+++ Prime.java
@@ -7,11 +7,14 @@ public class Prime {
     }
 
     private static boolean isPrime(int a) {
-        for (int i = 2; i < a; i++) {
+        // patrzymy czy liczba jest pierwsza poprzez przejście przez wszystkie liczby od 2 (1 nie jest l.p.) do jej połowy jej i sprawdzenie czy jest jej dzielnikiem
+        for (int i = 2; i < a/2; i++) {
+            // jeżeli liczba jest podzielna przez drugą bez reszty, to ta druga jest jej dzielnikiem, czyli liczba nie jest pierwsza
             if (a % i == 0) {
                 return false;
             }
         }
+        // jeżeli przeszlismy przez wszystkie liczby i żadna nie była dzielnikiem naszej liczby, zwracamy true, bo liczba jest pierwsza
         return true;
     }
 }
